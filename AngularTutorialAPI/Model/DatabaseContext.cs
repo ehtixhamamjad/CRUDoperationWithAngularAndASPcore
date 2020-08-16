8f2cf5346f64d68cb865e68a965fe60576772504
ehtixhamamjad <ehtixhamamjad@gmail.com>
2020-08-16 21:32:06 +0500

    FirstTime
    Complete

---
+++ AngularTutorialAPI/Model/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularTutorialAPI.Model
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions options):base (options)
+        {
+
+        }
+        /*Employee class set here*/
+        public DbSet<Employee> Employee { get; set; }
+
+    }
+}
---
+++ AngularTutorialAPI/Model/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AngularTutorialAPI.Model
+{
+    /*Creat a table in DataBase*/
+    public class Employee
+    {
+        /*Creat Property
+         short cuit of Property is prop and then double tab*/
+        public int id { get; set; }
+        public string name  { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+    }
+}
